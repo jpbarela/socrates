7d2692d95ea87b9885cc044dd9321592406b2744
J.P. Barela <barela@jpbarela.com>
2016-05-20 23:24:26 -0600

    Fix up some issues with the server script

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
+
+var app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/favorites', function(req, res){
+app.get('/favorites', function(req, res) {
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
@@ -25,8 +27,12 @@ app.get('favorites', function(req, res){
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-});
+}});
 
-app.list(3000, function(){
-  console.log("Listening on port 3000");
-});
+function start(config) {
+  app.listen(config.port);
+  console.log('Express server listening on port %d in %s mode', config.port, app.settings.env);
+}
+
+exports.start = start;
+exports.app = app;
